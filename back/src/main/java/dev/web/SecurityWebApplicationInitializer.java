d8f00aa4ec5bfdc8458666aa207f890c62eee656
Mounzer HADDARA <mounzer.haddara@hotmail.fr>
2017-06-14 12:04:26 +0200

    suppression page login spring security

--- back/src/main/java/dev/web/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package dev.web;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
