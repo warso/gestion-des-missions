f9f74c288765ed6e1fff6cb99828e7f78ed86a94
Quentin DANIEL <quentin.daniel7@gmail.com>
2017-06-14 17:13:04 +0200

    config mvc + GET /missions

--- back/src/main/java/dev/App.java
+++ back/src/main/java/dev/App.java
@@ -3,10 +3,14 @@ package dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Application démarrée via Spring Boot.
@@ -15,12 +19,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ServletComponentScan
 public class App {
 
-  /**
-   * Démarrage de l'application Web.
-   *
-   * @param args argument du programme
-   */
-  public static void main(String[] args) {
-    SpringApplication.run(App.class);
-  }
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+	/**
+	 * Démarrage de l'application Web.
+	 *
+	 * @param args
+	 *            argument du programme
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(App.class);
+	}
 }
---
+++ back/src/main/java/dev/controller/MissionController.java
@@ -0,0 +1,24 @@
+package dev.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import dev.model.Mission;
+import dev.repository.MissionRepo;
+
+@RestController
+public class MissionController {
+
+	@Autowired
+	private MissionRepo missionrepo;
+
+	@GetMapping("/missions")
+	public List<Mission> get() {
+ 
+		return missionrepo.findAll();
+	}
+}
