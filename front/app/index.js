3a57dd019e482143c217ff44fb0ae30aac95371d
Warsama ILMI AMIR <warsama.ilmiamir@gmail.com>
2017-06-14 14:27:13 +0200

    ajout constant API_URL

--- front/app/index.js
+++ front/app/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { route } from './app.route';
 import { AccueilComponent } from './accueil/accueil.component';
 import { NavBarComponent } from './navbar/navbar.component';
+const API_URL='http://localhost:8080'
 
 
 angular.module('app', [RouteModule])
---
+++ front/app/navbar/navbar.component.js
@@ -0,0 +1,13 @@
+import './navbar.component.css';
+import template from './navbar.component.html';
+
+class controller {
+    constructor () {
+    }
+}
+
+export let NavBarComponent = {
+    template,
+    controller,
+    bindings: {}
+};
