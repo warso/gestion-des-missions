59e6c47a062c0abf94eb2154fd5fcca27281d9f5
Mounzer HADDARA <mounzer.haddara@hotmail.fr>
2017-06-19 12:56:25 +0200

    M007 Missions Suppression ok avec la popup

--- front/app/mission/mission.component.js
+++ front/app/mission/mission.component.js
@@ -3,50 +3,61 @@ import template from './mission.component.html'
 import templateModal from './deletePopup.html'
 
 class controller {
-  constructor (MissionService, LoginService,$uibModal) {
+  constructor(MissionService, LoginService, $uibModal) {
     this.MissionService = MissionService
     this.LoginService = LoginService
     this.$uibModal = $uibModal
     this.missions = []
   }
 
-  $onInit () {
+  $onInit() {
+    this.rechargerMissions()
+  }
+
+  rechargerMissions() {
     let user = this.LoginService.loadUser()
     if (user !== undefined) {
       this.MissionService.getMissions(user.matricule)
-            .then(
-            missions => {
-              this.missions = missions
-            })
+        .then(
+        missions => {
+          this.missions = missions
+          console.log("Missions apres rechargement", this.missions)
+        })
     }
   }
 
-   DeleteButPopup(mission){
-      
-      
-        this.$uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            template: templateModal,
-            controller: function() {
-                 this.depart = "yo!"
-                 this.uneMission = mission;
-            },
-            controllerAs: '$ctrl',
+  popup(mission) {
+    // Ici je stoque mon this dans une variable, pour pouvoir l'utilisé dans une fonction anonyme qui a son propre this ( les fonctions anonyme ont leurs propres this)
+    let me = this;
+
+    me.$uibModal.open({
+      animation: true,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      template: templateModal,
+      controller: function ($scope, $uibModalInstance, MissionService) {
+        // je suis dans ma fonction anonyme et le this à partir de maintenant et jusqu'a ce qu'on sort de la focntion est celui de la fonction anonyme
+        this.depart = "yo!"
+        this.uneMission = mission;
+        this.test = function () {
+          console.log("test ok ")
+
+        }
+        this.annuler = () => {
+          $uibModalInstance.close()
+        }
+        this.validerSuppression = () => {
+          MissionService.deleteMission(this.uneMission.id)
+          // ici j'utilise mon this stoquer dans la variable me, car je suis dans une fonction anonyme avec son this propre
+            .then((rep) => me.rechargerMissions());
+        }
+      },
+      controllerAs: '$ctrlPop',
     });
-}
+  }
 
-closePopup() {
-  console.log("close popup")
-    $uibModalInstance.close($ctrl.selected.item);
-  };
 
-  cancel(){
-    console.log("cancel popup")
-    $uibModalInstance.dismiss('cancel');
-  };
-  
+
 }
 
 export let MissionComponent = {
