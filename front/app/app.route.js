16693025ca962adf7bbc56183d792d37c47e49c0
Warsama ILMI AMIR <warsama.ilmiamir@gmail.com>
2017-06-22 17:52:18 +0200

    Mofis app.route.js && package.json

--- front/app/app.route.js
+++ front/app/app.route.js
@@ -1,56 +1,53 @@
-export function route ($routeProvider, $locationProvider) {
+export function route($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true)
 
   $routeProvider
-        .when('/', {
-            // template: '<accueil></accueil>'
-          redirectTo: '/missionsVisualisation'
-        })
-        .when('/missionsVisualisation', {
-          template: '<mission></mission>',
-          requireAuth: true,
-          authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
-        })
-          .when('/frais/:id?', {
-          template: '<mission></mission>',
-          requireAuth: true,
-          authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
-        })
-	.when('/noteDeFrais', {
-          template: '<note-de-frais></note-de-frais>',
-          requireAuth: true,
-          authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
-        })
-        .when('/missionsPlanning', {
-          template: '<mission-planning></mission-planning>',
-          requireAuth: true,
-          authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
-        })
-	.when('/missionsModification/:id?',{
-          template:'<mission-modification></mission-modification>'
-        })
-        .when('/missionsValidation', {
-          template: '<mission-validation></mission-validation>',
-          requireAuth: true,
-          authorizeRole: ['MANAGER']
-        })
-        .when('/primes', {
-          template: '<primes></primes>',
-          requireAuth: true,
-          authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
-        })
-        .when('/missionsCreation', {
-          template: '<mission-creation></mission-creation>',
-          requireAuth: true,
-          authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
-        })
-        .when('/login', {
-          template: '<login-component></login-component>'
-        })
-        .when('/utilisateur', {
-          template: '<utilisateur></utilisateur>'
-        })
-        .otherwise({
-          redirectTo: '/'
-        })
+    .when('/', {
+      // template: '<accueil></accueil>'
+      redirectTo: '/missionsVisualisation'
+    })
+    .when('/missionsVisualisation', {
+      template: '<mission></mission>',
+      requireAuth: true,
+      authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
+    })
+    .when('/frais/:id?', {
+      template: '<mission></mission>',
+      requireAuth: true,
+      authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
+    })
+    .when('/noteDeFrais', {
+      template: '<note-de-frais></note-de-frais>',
+      requireAuth: true,
+      authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
+    })
+    .when('/missionsPlanning', {
+      template: '<mission-planning></mission-planning>',
+      requireAuth: true,
+      authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
+    })
+    .when('/missionsModification/:id?', {
+      template: '<mission-modification></mission-modification>'
+    })
+    .when('/missionsValidation', {
+      template: '<mission-validation></mission-validation>',
+      requireAuth: true,
+      authorizeRole: ['MANAGER']
+    })
+    .when('/primes', {
+      template: '<primes></primes>',
+      requireAuth: true,
+      authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
+    })
+    .when('/missionsCreation', {
+      template: '<mission-creation></mission-creation>',
+      requireAuth: true,
+      authorizeRole: ['EMPLOYE', 'ADMINISTRATEUR', 'MANAGER']
+    })
+    .when('/login', {
+      template: '<login-component></login-component>'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
 }
